e966c0bf33ddaa9c3053b8c28a6ee7ab830cd1a9
xaveryh <verillytmt@gmail.com>
2023-08-05 16:19:34 +0800

    Added micromodal to html Styled timezone Added modal function on js

--- static/index.js
+++ static/index.js
@@ -1,12 +1,17 @@
 function updateTime() {
-        var time = dayjs().format("HH:mm:ss")
-        document.getElementById("time").innerHTML = time
+    var time = dayjs().format("HH:mm:ss")
+    document.getElementById("time").innerHTML = time
 }
 
+function showModal(element) {
+    micromodal.show(element )
+}
 
 const dayjs = require("dayjs")
+const MicroModal = require("micromodal")
 var utc = require("dayjs/plugin/utc")
 var timezone = require("dayjs/plugin/timezone")
+
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
@@ -24,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     var timeHTML = document.getElementById("time")
 
     userTimezone.innerHTML = dayjs.tz.guess()
+    userTimezone.addEventListener("click", () => { showModal(userTimezone) })
     dateHTML.innerHTML = date
     timeHTML.innerHTML = time
 })
