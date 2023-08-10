9cfcdaa20ca2fa4abab116fb3a8daa795f59d8ff
verillyh <verillytmt@gmail.com>
2023-08-11 01:09:16 +0800

    Added selectizejs to node_modules Finished select with search feature Updated timezone display after changing preferred timezone TODO: -- Change time when timezone is affected

--- static/index.js
+++ static/index.js
@@ -4,6 +4,15 @@ function updateTime() {
     $("#time").html(time);
 }
 
+function updatePage() {
+    var selectedTimezone = $("#timezone-list").selectize()[0].selectize.getValue();
+    selectedTimezone = selectedTimezone.split(" ")[0];
+    var currentTimezone = $("#current-timezone");
+
+    $(currentTimezone).html(selectedTimezone)
+    
+}
+
 function triggerModal() {
     micromodal.show("modal-1")
 }
@@ -11,26 +20,32 @@ function triggerModal() {
 function insertTimezones() {
     var selectHTML = $("#timezone-list")
     var timezoneList = Intl.supportedValuesOf("timeZone")
+
     timezoneList.forEach( e => {
-        newOption = $("<option class='timezone-option'>" + e + "</option>")
+        // Get current based on current timezone chosen per loop, and format to string
+        let offset = dayjs().tz(e).format("Z").toString()
+        // Format offset to be "GMT +00:00"
+        offset = "GMT " + offset
+        
+        // Format options to be "{timezone} {offset}"
+        newOption = $("<option class='timezone-option'>" + e + " " + offset + "</option>")
         $(selectHTML).append(newOption)
     })
 }
 
-function openTimezoneList() {
-    var timezoneList = document.getElementById("timezone-list")
-    timezoneList.style.display = "block"
-}
-
-function closeTimezoneList() {
-    var timezoneList = document.getElementById("timezone-list")
-    timezoneList.style.display = "none"
+function selectizeTzList() {
+    var timezoneList = $("#timezone-list")
+    $(timezoneList).selectize({
+        allowEmptyOption: true,
+        showEmptyOptionInDropdown: true
+    })
 }
 
 /* ---------- NPM Packages ----------- */
 const dayjs = require("dayjs")
 const micromodal = require("../node_modules/micromodal/dist/micromodal")
 const $ = require("../node_modules/jquery/dist/jquery")
+const selectize = require("@selectize/selectize")
 var utc = require("dayjs/plugin/utc")
 var timezone = require("dayjs/plugin/timezone")
 
@@ -52,30 +67,21 @@ $("document").ready(() => {
     var userTimezone = $("#current-timezone");
     var dateHTML = $("#date");
     var timeHTML = $("#time");
-    var searchTimezone = $("#timezone-search")
-    var timezoneList = $("#timezone-list")
-    
+    var submitTimezone = $("#modal-1__submit");
+
     // Default timezone
     $(userTimezone).html(dayjs.tz.guess());
     
     // Click to trigger modal -> change timezone
     $(userTimezone).click(triggerModal);
 
-    // On search bar's focus event, toggle list of available timezones
-    $(searchTimezone).on("focus", openTimezoneList);
-    $(searchTimezone).on("blur", () => {
-        if (document.activeElement.id == "timezone-list") {
-            openTimezoneList()
-        }
-        else {
-            closeTimezoneList()
-        }
-    })
-    $(timezoneList).on("focusout", closeTimezoneList)
+    // When timezone entered, update time and current timezone
+    $(submitTimezone).click(updatePage)
 
     // Insert date and time
     $(dateHTML).html(date);
     $(timeHTML).html(time);
     
     insertTimezones()
+    selectizeTzList()
 })
